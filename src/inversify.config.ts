9730d8cf1042798e6d8defe922fdfc6f894767fb
mikifriki <mihkel@miq.ee>
2024-06-19 23:23:02 +0300

    Initial try of removing Spotify implementation

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -12,7 +12,6 @@ import PlayerManager from './managers/player.js';
 import AddQueryToQueue from './services/add-query-to-queue.js';
 import GetSongs from './services/get-songs.js';
 import YoutubeAPI from './services/youtube-api.js';
-import SpotifyAPI from './services/spotify-api.js';
 
 // Commands
 import Command from './commands/index.js';
@@ -38,7 +37,6 @@ import Skip from './commands/skip.js';
 import Stop from './commands/stop.js';
 import Unskip from './commands/unskip.js';
 import Volume from './commands/volume.js';
-import ThirdParty from './services/third-party.js';
 import FileCacheProvider from './services/file-cache.js';
 import KeyValueCacheProvider from './services/key-value-cache.js';
 
@@ -61,7 +59,6 @@ container.bind<PlayerManager>(TYPES.Managers.Player).to(PlayerManager).inSinglet
 container.bind<GetSongs>(TYPES.Services.GetSongs).to(GetSongs).inSingletonScope();
 container.bind<AddQueryToQueue>(TYPES.Services.AddQueryToQueue).to(AddQueryToQueue).inSingletonScope();
 container.bind<YoutubeAPI>(TYPES.Services.YoutubeAPI).to(YoutubeAPI).inSingletonScope();
-container.bind<SpotifyAPI>(TYPES.Services.SpotifyAPI).to(SpotifyAPI).inSingletonScope();
 
 // Commands
 [
