18b125cbb4f0436de0a78b5c1d5698e85f1dee2c
mikifriki <mihkel@miq.ee>
2024-06-19 23:23:02 +0300

    Initial try of removing Spotify implementation

--- src/utils/get-youtube-suggestions-for.ts
+++ src/utils/get-youtube-suggestions-for.ts
@@ -1,6 +1,7 @@
+import {APIApplicationCommandOptionChoice} from 'discord-api-types/v10';
 import got from 'got';
 
-const getYouTubeSuggestionsFor = async (query: string): Promise<string[]> => {
+const getuggestions = async (query: string): Promise<string[]> => {
   const [_, suggestions] = await got('https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=', {
     searchParams: {
       client: 'firefox',
@@ -8,6 +9,25 @@ const getYouTubeSuggestionsFor = async (query: string): Promise<string[]> => {
       q: query,
     },
   }).json<[string, string[]]>();
+
+  return suggestions;
+};
+
+const getYouTubeSuggestionsFor = async (query: string, limit = 10): Promise<APIApplicationCommandOptionChoice[]> => {
+  const [youtubeSuggestions] = await Promise.all([
+    getuggestions(query),
+  ]);
+
+  const suggestions: APIApplicationCommandOptionChoice[] = [];
+
+  suggestions.push(
+    ...youtubeSuggestions
+      .slice(0, 10)
+      .map(suggestion => ({
+        name: `YouTube: ${suggestion}`,
+        value: suggestion,
+      }),
+      ));
 
   return suggestions;
 };
